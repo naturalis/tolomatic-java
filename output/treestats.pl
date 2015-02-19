55ca89d2ba7a1d284d494cc5e7bdc37a38f9b17b
rvosa <rutgeraldo@gmail.com>
2015-02-19 13:37:21 +0100

    various fixes

--- output/treestats.pl
+++ output/treestats.pl
@@ -1,53 +1,101 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Cwd;
+use File::Find;
 use Getopt::Long;
 use List::Util 'sum';
 use Bio::Phylo::IO 'parse_tree';
+use Bio::Phylo::Util::Logger ':levels';
+
+# store current working directory for File::Find
+my $cwd = getcwd();
 
 # process command line arguments
-my $ext = 'txt';
-my $indir = '.';
-my @properties = qw(
-	tree_length
-	tree_height
-	number_of_terminals
-	imbalance
-	i2
-	gamma
-	fiala_stemminess
-	rohlf_stemminess
-	fp
-	es
-	pe
-	shapley	
-	avtd
-);
+my ( $find, $infile );
+my ( $ext, $verbosity, $type ) = ( 'tsv', WARN, 'adjacency' );
+
+# possible properties:
+# 	tree_length
+# 	tree_height
+# 	number_of_terminals
+# 	imbalance
+# 	i2
+# 	gamma
+# 	rohlf_stemminess
+# 	fp
+# 	es
+# 	pe
+# 	shapley	
+# 	avtd
+my @properties = qw(fiala_stemminess);
 GetOptions(
-	'ext=s'   => \$ext,
-	'indir=s' => \$indir,
-	'prop=s'  => \@properties,
+	'ext=s'    => \$ext,
+	'infile=s' => \$infile,
+	'prop=s'   => \@properties,
+	'find'     => \$find,
+	'verbose+' => \$verbosity,
+	'type=s'   => \$type,
+);
+
+# instantiate logger
+my $log = Bio::Phylo::Util::Logger->new(
+	'-level' => $verbosity,
+	'-class' => [ 'main', 'Bio::Phylo::Parsers::Adjacency' ],
 );
 
 # print header
 $|++;
 print join( "\t", 'file', @properties ), "\n";
 
-# start reading
-opendir my $dh, $indir or die $!;
-while( my $entry = readdir $dh ) {
-	next if $entry =~ /^\.\.?$/;
-	next if $entry !~ /\.$ext$/;
+# start processing
+if ( $find ) {	
 
-	# create path
-	my $file = $indir . '/' . $entry;
+	# start recursing
+	$log->info("going to scan for $ext files recursively, starting at $indir");
+	find(\&wanted,$indir);
+}
+else {
+
+	# start reading
+	$log->info("going to scan for $ext files in $indir");
+	opendir my $dh, $indir or die $!;
+	while( my $entry = readdir $dh ) {
+		next if $entry =~ /^\.\.?$/;
+		next if $entry !~ /\.$ext$/;
+
+		# process file
+		my $file = $indir . '/' . $entry;
+		eval { do_stats($infile) };
+		if ( $@ ) {
+			$log->error("Problem with $file: $@");
+		}		
+	}
+}
+
+sub wanted {
+	if ( /\.$ext$/ ) {
 	
+		# process file
+		my $file = $cwd . '/' . $File::Find::name;
+		eval { do_stats($file) };
+		if ( $@ ) {
+			$log->error("Problem with $file: $@");
+		}
+	}
+}
+
+sub do_stats {
+	my $file = shift;
+	$log->info("going to compute tree stats for $file");
+
 	# read tree
 	my $tree = parse_tree(
-		'-format'     => 'newick',
+		'-format'     => $type,
 		'-file'       => $file,
 		'-as_project' => 1,
 	);
+	$log->info("done reading $type data from $file");
 	$tree->scale(1);
 	
 	# count tips to average over
@@ -57,11 +105,13 @@ while( my $entry = readdir $dh ) {
 	if ( not $tree->is_binary ) {
 		$tree->resolve;
 		$file .= '*';
+		$log->warn("tree needed to be resolved, marked with *");
 	}
 	my @result = ( $file );
 	
 	# calculate results
 	for my $p ( @properties ) {
+		$log->info("calculating $p");
 		my $method = 'calc_' . $p;
 		my $val = $tree->$method;
 		
@@ -76,4 +126,5 @@ while( my $entry = readdir $dh ) {
 	
 	# print output
 	print join( "\t", @result ), "\n";
+
 }
