64ea8209373833ea90d7c6ef954c023726ad5ff0
rvosa <rutgeraldo@gmail.com>
2015-02-19 14:39:15 +0100

    updated sampler script to do jackknifing

--- script/sampler.sh
+++ script/sampler.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
 PRIMERS="F04R22 NF118Sr2b"
-TREATMENTS="pre post"
-DATA=../data/bik/raw
+SITES="BayfrontPark BelleairBlvd RyanCt ShellfishLab"
+DATA=../data/bik
+OUTPUT=../output
+ROOTING=midroot
 REPLICATES=100
-STATS=treestats.tsv
+STATS=${OUTPUT}/treestats-jackknife.tsv
 
 # iterate over primer sets
 for PRIMER in $PRIMERS; do
 
-	# iterate over pre- and post treatment within primer set
-	for TREATMENT in $TREATMENTS; do
+	# get the tree name
+	TREE=${DATA}/trees/${PRIMER}-${ROOTING}.db
+
+	# iterate over the sites that have both pre and post treatment
+	for SITE in $SITES; do
 	
-		# iterate over the sites
-		LISTS=`ls ${PRIMER}/${TREATMENT}/*.lst`
-		for LIST in $LISTS; do
+		# get the site's pre- and post treatment's file name
+		PRE_LIST=${DATA}/presence_lists/${PRIMER}/pre/${SITE}.lst
+		POST_LIST=${DATA}/presence_lists/${PRIMER}/post/${SITE}.lst
 		
-			# calculate number of lines to sample
-			LENGTH=`wc -l ${LIST}`
+		# calculate number of lines to sample
+		LENGTH=`wc -l ${POST_LIST}`
+		
+		# create directory for replicates
+		OUTDIR=${OUTPUT}/jackknife/${PRIMER}/${SITE}
+		mkdir ${OUTDIR}
+		
+		# create replicates
+		for (( i=1; i <= REPLICATES; i++ )); do
+		
+			# make a shorter name stem
+			OUTSTEM=${OUTDIR}/${i}
 			
-			# create directory for replicates
-			STEM=`echo $LIST | sed -e 's/.lst//'`
-			mkdir ${STEM}
+			# sample the taxon list
+			sampler -s ${LENGTH} -i ${PRE_LIST} > ${OUTSTEM}.lst
 			
-			# create replicates
-			for (( i=1; i <= REPLICATES; i++ )); do
+			# prune the tree from Megatree database, verbose, with tabular output
+			prune_megatree -d ${TREE} -i ${OUTSTEM}.lst -v -t > ${OUTSTEM}.tsv
 			
-				# make shorter names
-				OUTSTEM=${STEM}/${i}
-				INSTEM=${DATA}/${PRIMER}
-				
-				# sample the taxon list
-				sampler -s $LENGTH -i ${INSTEM}.txt > ${OUTSTEM}.lst
-				
-				# prune the tree from Megatree database, verbose, with tabular output
-				prune_megatree -d ${INSTEM}.db -i ${OUTSTEM}.lst -v -t > ${OUTSTEM}.tsv
-				
-				# compute tree statistics. we're writing to the same file multiple
-				# times, therefore append ***BE AWARE OF THIS***
-				perl treestats-file.pl -v -i ${OUTSTEM}.tsv >> ${STATS}
-			done  
-		done
+			# compute tree statistics. we're writing to the same file multiple
+			# times, therefore append ***BE AWARE OF THIS***
+			perl treestats-file.pl -v -i ${OUTSTEM}.tsv >> ${STATS}
+		done  
 	done
 done
