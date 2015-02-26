ed9b3313fc3b47b1421b47b923ac4c3c3e4778ae
Rutger Vos <rutgeraldo@gmail.com>
2015-02-26 08:16:50 +0100

    different avtd-ish algorithm

--- script/taxon_avtd.pl
+++ script/taxon_avtd.pl
@@ -34,7 +34,8 @@ my $log  = Bio::Phylo::Util::Logger->new(
 
 # read pre and post lists
 my %class;
-$log->info("going to read pre and post files ($pre and $post)");
+$log->info("going to read pre file ($pre)");
+$log->info("going to read post file ($post)");
 $class{$_} = 'pre'  for read_file( $pre,  'chomp' => 1 );
 $class{$_} = 'post' for read_file( $post, 'chomp' => 1 );
 
@@ -64,16 +65,13 @@ my $qtree = {};
 
 # analyze tree shape
 $log->info("going to analyze tree shape");
+say join "\t", qw(name replicate pre post n);
 recurse( 'root' => $qtree );
 sub recurse {
 	my ( $name, $node ) = @_;
 	
 	# bins for pre and post children of focal node
-	my ( @pre, @post );
-	my %set = ( 
-		'pre'  => \@pre, 
-		'post' => \@post,
-	);
+	my %set = ( 'pre'  => [], 'post' => [] );
 	
 	# iterate over focal node's children
 	while ( my ( $key, $value ) = each %{ $node } ) {
@@ -83,26 +81,42 @@ sub recurse {
 	}
 	
 	# do rarefaction avtd if both bins are populated
-	if ( @pre >= 2 and @post >= 2 ) {
-		$log->info("going to do AVTD for $name");
-		avtd( $name, \@pre, \@post );
+	my $pre_size  = scalar( @{ $set{'pre'} } );
+	my $post_size = scalar( @{ $set{'post'} } );
+	if ( $pre_size >= 2 and $post_size >= 2 and $name ne 'environmental' ) {
+		$log->info("going to do AVTD for $name (pre: $pre_size, post: $post_size)");
+		$log->debug(Dumper(\%set));
+		avtd( $name, $set{'pre'}, $set{'post'} );
 	}
+}
+
+sub get_nodes {
+	return grep { defined $_ } map { $tree->_rs->search({ 'name' => $_ })->single } @_;
 }
 
 # do average taxonomic distance
 sub avtd {
 	my ( $name, $pre, $post ) = @_;
-	my @pre  = grep { defined $_ } map { $tree->_rs->find($_) } @{ $pre  };
-	my @post = grep { defined $_ } map { $tree->_rs->find($_) } @{ $post };
+	my @pre  = get_nodes( @{ $pre  } );
+	my @post = get_nodes( @{ $post } );
+	$log->info("pre nodes: ".scalar(@pre));
+	$log->info("post nodes: ".scalar(@post));
 	for my $i ( 1 .. $reps ) {
+		my ( @prd, @pod );
 		
-		# make shuffled, rarefacted copies
+		# make shuffled, even-sized copies
 		my ($max) = sort { $a <=> $b } scalar(@pre), scalar(@post);
-		my @pre_copy  = shuffle @pre;
-		my @post_copy = shuffle @post;
-		@pre_copy  = splice @pre_copy,  $max if @pre_copy  > $max;
-		@post_copy = splice @post_copy, $max if @post_cope > $max
+		my @pr = shuffle @pre;
+		my @po = shuffle @post;
+		splice @pr, $max if @pr > $max;
+		splice @po, $max if @po > $max;
 		
-			
+		# compute heights
+		for my $j ( 0 .. $#pr ) {
+			my $height = $pr[$j]->get_mrca( $po[$j] )->height;		
+			push @prd, ( $pr[$j]->height - $height );
+			push @pod, ( $po[$j]->height - $height );
+		}
+		say join "\t", $name, $i, sum(@prd)/scalar(@prd), sum(@pod)/scalar(@pod), $max;
 	}
 }
