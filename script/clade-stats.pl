06448cb367b91849c048311ac59c03638c1d249f
rvosa <rutgeraldo@gmail.com>
2015-03-10 10:55:49 +0100

    'clade stats' selected terminal triplets to compute summary statistics over. visualization (*_n3.svg, PCA.pdf) shows that post is more stemmy

--- script/clade-stats.pl
+++ script/clade-stats.pl
@@ -7,12 +7,13 @@ use Template;
 use File::Slurp;
 use Getopt::Long;
 use Statistics::Descriptive;
-use List::Util qw'shuffle sum';
-use Bio::Phylo::IO 'parse_tree';
+use List::Util qw'shuffle sum min';
+use Bio::Phylo::IO qw'parse_tree parse';
 use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # these are the fields that will end up in the output. the ones prefixed with
-# 'stat' are methods of the Statistics::Descriptive::Full object; the ones 
+# 'dist' and 'hght' are methods of the Statistics::Descriptive::Full object; the ones 
 # prefixed with 'tree' are methods of the Bio::Phylo::Forest::Tree object;
 # the others are defined in the call to the Template::Toolkit processor
 my @fields = (
@@ -21,15 +22,24 @@ my @fields = (
 	'post', 
 	'class', 
 	'id', 
-	'stat.count',
-	'stat.mean',
-	'stat.variance',
-	'stat.standard_deviation',
-	'stat.min',
-	'stat.max',
-	'stat.median',
-	'stat.harmonic_mean',
-	'stat.geometric_mean',
+	'dist.count',
+	'dist.mean',
+	'dist.variance',
+	'dist.standard_deviation',
+	'dist.min',
+	'dist.max',
+	'dist.median',
+	'dist.harmonic_mean',
+	'dist.geometric_mean',
+	'hght.count',
+	'hght.mean',
+	'hght.variance',
+	'hght.standard_deviation',
+	'hght.min',
+	'hght.max',
+	'hght.median',
+	'hght.harmonic_mean',
+	'hght.geometric_mean',	
 	'tree.calc_tree_length',
 	'tree.calc_redundancy',
 	'tree.calc_gamma',
@@ -45,19 +55,22 @@ my @fields = (
 
 # process command line arguments
 my $verbosity = WARN;
-my ( $infile, $pre, $post, $cut, $db );
+my ( $infile, $pre, $post, $cut, $db, $thresh, $blastdb );
 GetOptions(
-	'db=s'     => \$db,
-	'infile=s' => \$infile,
-	'pre=s'    => \$pre,
-	'post=s'   => \$post,
-	'cut=i'    => \$cut,
-	'verbose+' => \$verbosity,
+	'db=s'      => \$db,
+	'infile=s'  => \$infile,
+	'pre=s'     => \$pre,
+	'post=s'    => \$post,
+	'cut=i'     => \$cut,
+	'thresh=f'  => \$thresh,
+	'blastdb=s' => \$blastdb,
+	'verbose+'  => \$verbosity,
 );
 
 # instantiate helper objects
 my $tmpl = Template->new;
-my $stat = Statistics::Descriptive::Full->new;
+my $dist = Statistics::Descriptive::Full->new;
+my $hght = Statistics::Descriptive::Full->new;
 my $tree = parse_tree(
 	'-format' => 'newick',
 	'-file'   => $infile,
@@ -140,6 +153,18 @@ for my $key ( keys %bin ) {
 			$log->info(scalar(@tips). ">$cut, ignoring");
 			next MRCA;
 		}
+		if ( @tips < 3 ) {
+			$log->info(scalar(@tips). "<3, ignoring");
+			next MRCA;
+		}
+		
+		# compute seq dist. if $seqdist < $thresh we assume this is an
+		# intraspecific clade and we don't want it
+		my $seqdist = compute_seqdist(@tips);
+		if ( $seqdist < $thresh ) {
+			$log->info("$seqdist<$thresh, ignoring");
+			next MRCA;
+		}
 		
 		# collect all pairwise distances among subtended tips
 		$log->info("calculating pairwise distances among ".scalar(@tips)." nodes");
@@ -150,30 +175,78 @@ for my $key ( keys %bin ) {
 			}
 		}
 		
+		# collect all heights among subtended nodes
+		$log->info("calculating heights among subtended nodes");
+		my $subtree = make_subtree(@tips);
+		my %height;
+		$subtree->visit_depth_first(
+			'-pre' => sub {
+				my $node = shift;
+				my $id = $node->get_id;
+				if ( $node->is_root ) {
+					$height{$id} = $node->get_branch_length;					
+				}
+				else {
+					my $parent = $node->get_parent;
+					my $pid = $parent->get_id;
+					$height{$id} = $height{$pid} + $node->get_branch_length;
+				}
+			}
+		);
+		
 		# produce output
-		$stat->add_data(@dist);
+		$dist->add_data(@dist);
+		$hght->add_data(values %height);
 		$tmpl->process( \$template, {
 			'infile' => $infile,
 			'pre'    => $pre,
 			'post'   => $post,
 			'class'  => $key,
 			'id'     => $mrca->get_id,
-			'stat'   => $stat,
-			'tree'   => make_subtree(@tips),
+			'dist'   => $dist,
+			'hght'   => $hght,
+			'tree'   => $subtree,
 		}) || $log->error($tmpl->error);
-		$stat->clear;		
+		$dist->clear;
+		$hght->clear;		
 	}
 }
 
+sub compute_seqdist {
+	my @tips = @_;
+	my $ids = join ',', map { "'lcl|$_'" } map { $_->get_name } @tips;
+	
+	# get alignment object
+	my $fasta = `blastdbcmd -db $blastdb -entry $ids | muscle -quiet`;
+	my ($matrix) = @{ parse(
+		'-type'   => 'dna',
+		'-format' => 'fasta',
+		'-string' => $fasta,
+		'-as_project' => 1,
+	)->get_items(_MATRIX_) };
+	
+	# compute pairwise distances
+	my @rows = @{ $matrix->get_entities };
+	my @dist;
+	for my $i ( 0 .. $#rows - 1 ) {
+		for my $j ( $i + 1 .. $#rows ) {
+			push @dist, $rows[$i]->calc_distance($rows[$j]);
+		}
+	}
+	my $min = min @dist;
+	$log->info("min dist: $min");
+	return $min;
+}
+
 sub make_subtree {
-	my ( @tips ) = @_;
+	my @tips = @_;
 	my $list = join ',', map { $_->get_name } @tips;
 	my $newick = `prune_megatree -db $db -list $list`;
 	my $tree = parse_tree(
 		'-format' => 'newick',
 		'-string' => $newick,
 	);
-	$tree->get_root->set_branch_length(0);
+	#$tree->get_root->set_branch_length(0);
 	$tree->scale(1);
 	return $tree;
 }
--- script/clade-stats.sh
+++ script/clade-stats.sh
@@ -6,19 +6,21 @@ DATA=../data/bik
 OUTPUT=../output
 ROOTING=midroot
 CUT=6
+THRESH=0.01
+BLASTDB=${DATA}/raw/merged.fasta
+STATS=${OUTPUT}/clade-stats-dist0.99.tsv
+LOG=${OUTPUT}/clade-stats-dist0.99.log
 
-STATS=${OUTPUT}/clade-stats.tsv
-LOG=${OUTPUT}/clade-stats.log
 echo '' > $STATS
 echo '' > $LOG
 
-COMMAND="perl clade-stats.pl"
+COMMAND="perl clade-stats.pl -v -c $CUT -t $THRESH -b $BLASTDB"
 for PRIMER in $PRIMERS; do	
 	DB=${DATA}/trees/${PRIMER}-${ROOTING}.db
 	for SITE in $SITES; do
 		TREE=${DATA}/trees/${PRIMER}-${SITE}-${ROOTING}.dnd
 		PRE=${DATA}/presence_lists/${PRIMER}/pre/${SITE}.lst
 		POST=${DATA}/presence_lists/${PRIMER}/post/${SITE}.lst
-		$COMMAND -i $TREE -db $DB -pre $PRE -post $POST -v -c $CUT >> $STATS 2>> $LOG
+		$COMMAND -i $TREE -db $DB -pre $PRE -post $POST >> $STATS 2>> $LOG
 	done
 done
